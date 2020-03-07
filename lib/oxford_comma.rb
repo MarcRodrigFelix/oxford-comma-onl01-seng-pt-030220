f7a3ccbe318a4b651681d33e12f2df62b8158c14
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2020-03-07 01:56:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,14 @@
 require 'pry'
 
 def oxford_comma(array)
-  
-  # array.join
-  
-  # array.join(" and ")
-  
+
     if array.length == 1
      return array.join
-    elsif array.length == 2 
+    elsif array.length == 2
      return array.join(" and ")
-    elsif array.length >= 3 
-      array[-1].insert(0,"and ")
+    else
+      array[-1].insert(0,"and ") #add "and " to the begining of last array index
     end
-  
-  return array.join(", ")
+
+  return array.join(", ") #join all array index into string with commas.
 end
